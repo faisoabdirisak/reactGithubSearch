0ba7a4911d3dc0e26227939f0d3a758913db5174
faisoabdirisak <faisoabdirisak@gmail.com>
2022-04-19 14:37:32 +0300

    adds react projects

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import './App.css';
+import DispalyData from "./Display";
+
+function App() {
+  const [data, setData] = useState({});
+  const [username, setUsername] = useState('');
+  const [repositories,setRepositories]=useState([]);
+
+   
+
+  const onChangeHandle=(e)=>{
+    setUsername(e.target.value);
+  }
+  const submitHandler=async e=>{
+    e.preventDefault();
+    const profile = await fetch(`https://api.github.com/users/${username}`);
+   
+    const profileJson= await profile.json();
+    // console.log(profileJson)
+    const repositories= await fetch(profileJson.repos_url);
+    const repoJson =await repositories.json();
+    console.log(repoJson);
+    if (profileJson){
+      setData(profileJson);
+      setRepositories(repoJson)
+    }
+   
+  };
+  return (
+    <>
+     <div class="container">
+        <div class="row justify-content-center pt-5 searchblock">
+            <div class="col-12 col-md-10 col-lg-8">
+                <div id="message"></div>
+                    <h2>Github Search</h2>
+                <form id="searchForm" class="card card-sm" method="POST">
+                    <div class="card-body row no-gutters align-items-center">
+                         <div class="col-auto">
+                            <i class="fas fa-search h4 text-body"></i>
+                        </div>
+                      
+                        <div class="col">
+                            <input class="form-control form-control-lg form-control-borderless" type="search"
+                                placeholder="Search Github Profile" 
+                                autocomplete="off" value={username} onChange={onChangeHandle}/>
+                        </div>
+                    
+                        <div class="col-auto">
+                            <button class="btn btn-lg btn-success ml-9" type="submit" onClick={submitHandler}>Search</button>
+                        </div>
+                    
+                    </div>
+                </form>
+                 <DispalyData data={data} repositories={repositories}/>
+            </div>
+          
+        </div>
+        </div>
+     {/* <h2>Github Search</h2>
+    <div className="git">
+     
+  <div className="form">
+  <input className="input" class="form-control" placeholder="Search Github User" 
+  type="text" value={username} onChange={onChangeHandle}/>
+  <button type="submit" class="btn btn-primary justify-content-center" onClick={submitHandler}>Submit</button>
+   
+    </div>
+    </div> */}
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/Display.js
@@ -0,0 +1,80 @@
+import './App.css';
+const DispalyData = ({data, repositories}) => {
+    return ( 
+        <>
+        <div class="container">
+             <div className="profile">
+            <div class="row">
+                <div class="col-md-3">
+                    <div className="profile-sidebar">
+                        <div id="profile"> 
+                        <div className="profile-userpic">
+                              {!data.avatar_url?(""):(<img className="d-block"
+                                    src={data.avatar_url}
+                                    alt={data.avatar_url}/>)}
+                            <div className='profile-usertitle'>
+                                <div className="profile-usertitle-name">
+                                    {data.name}
+                                </div>
+                                 <div className="profile-usertitle-job">
+                                     {data.login}
+                                 </div>
+                            </div>
+                               <div class="portlet light bordered">
+
+            <div class="row list-separated profile-stat">
+            <div class="col-md-6 col-sm-6 col-xs-6" >
+                 <div class="uppercase profile-stat-title">{data.followers}</div>
+                 <div class="uppercase profile-stat-text">Followers</div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="uppercase profile-stat-title">{data.following}</div>
+            <div class="uppercase profile-stat-text">Following</div>
+            </div>
+            </div>
+            <div><h4 className="profile-desc-title">About </h4>
+        <span className="profile-desc-text">{data.bio}</span></div>
+        
+            </div>
+                        </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div className="profile-content">
+                      
+                        <ol class="d-flex flex-wrap list-style-none gutter-condensed mb-4" id="repos">
+                            <li class="mb-3 d-flex flex-content-stretch col-12 col-md-6 col-lg-6">
+            <div class="card" className='card1'>
+              <div class="card-body">
+              <div class="card-title">
+                  <h5>Repositories</h5>
+                   <p>{repositories.map(repo=>(
+                <div key={repo.name}>
+                    <h5><a target="_blank" href="{repo.html_url}">{repo.name}</a></h5>
+                     <p><a href={repo.html_url} className="repo" >{repo.html_url}</a></p>
+                     <p class="card-text">{repo.description }</p>
+                       <i class="fas fa-circle">{repo.language}</i> 
+
+                     </div>
+               
+            ))}</p>
+                 
+            </div>
+            </div>
+            </div>
+            </li>
+                        </ol>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+        
+        </div>
+        </>
+     );
+}
+ 
+export default DispalyData;
